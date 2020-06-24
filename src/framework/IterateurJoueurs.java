d35e6c85d387cd7c16f3585dcd1aef40825cf220
wolfie <ugo.costantini3@etu.univ-lorraine.fr>
2020-06-24 13:40:42 -0400

    Ajout javadoc classes iterateur

--- src/framework/IterateurJoueurs.java
+++ src/framework/IterateurJoueurs.java
@@ -2,6 +2,9 @@ package framework;
 
 import java.util.Iterator;
 
+/**
+ * Iterateur sur une collection de joueurs
+ */
 public class IterateurJoueurs implements Iterator<Joueur> {
     @Override
     public boolean hasNext()
